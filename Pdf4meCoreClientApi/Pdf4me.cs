5575b5faae425bff10b040e1b2bd60ee8ba299b0
Varun Satish <varun.satish@ynoox.in>
2018-11-23 15:14:37 +0530

    updating from swag and adding additional wrappers

--- Pdf4meCoreClientApi/Pdf4me.cs
+++ Pdf4meCoreClientApi/Pdf4me.cs
@@ -11,57 +11,87 @@ using System.Threading.Tasks;
 
 namespace Pdf4meClient
 {
-    public class Pdf4meClient
+    public class Pdf4me
     {
 
-        //public static readonly Pdf4meClient Instance = new Pdf4meClient();
+        public static readonly Pdf4me Instance = new Pdf4me();
 
-        //string _api = "https://api-dev.pdf4me.com";
+        string _api = "https://api-dev.pdf4me.com";
         string _authString = "https://login.microsoftonline.com/devynooxlive.onmicrosoft.com";
-        // string _clientId = "";
-        // string _key = "";
-        private AuthConfig _authConfig;
-        private HttpClient _httpClient;
+        string _clientId = "";
+        string _key = "";
+        string _basicToken = "";
+
+        static Pdf4me()
+        {
+
+
+        }
 
         // Instance constructor is private to enforce singleton semantics
-        public Pdf4meClient(AuthConfig authConfig) 
+        private Pdf4me() : base()
         {
-            _authConfig = authConfig;
             //_clientId = ConfigurationManager.AppSettings["Pdf4meClientId"];
             //_key = ConfigurationManager.AppSettings["Pdf4meSecret"];
         }
 
+        /// <summary>
+        /// Initialise with Client Id and Key
+        /// </summary>
+        /// <param name="clientId">ClientId</param>
+        /// <param name="key">Secret</param>
+        /// <param name="api">Api url to be set. If passed null, url will https://api-dev.pdf4me.com</param>
+        public void Init(string clientId, string key, string api)
+        {
+            _clientId = clientId;
+            _key = key;
 
+            if (!string.IsNullOrEmpty(api))
+            {
+                _api = api;
+            }
 
-        public DocumentClient DocumentClient
+            _basicToken = "";
+        }
+
+        /// <summary>
+        /// Initialise with basic token
+        /// </summary>
+        /// <param name="basicToken">Token</param>
+        /// <param name="api">Api url to be set. If passed null, url will https://api-dev.pdf4me.com</param>
+        public void Init(string basicToken, string api)
+        {
+            _basicToken = basicToken;
+
+            if (!string.IsNullOrEmpty(api))
+            {
+                _api = api;
+            }
+            _clientId = "";
+            _key = "";
+        }
+
+        public ConvertClient ConvertClient
+        {
+            get
+            {
+                return new ConvertClient(getApi());
+            }
+        }
+
+        /*public DocumentClient DocumentClient
         {
             get
             {
                 return new DocumentClient(getApi());
             }
-        }
+        }*/
 
-        public ManagementClient ManagementClient
+        public ExtractClient ExtractClient
         {
             get
             {
-                return new ManagementClient(getApi());
-            }
-        }
-
-        public OptimizeClient OptimizeClient
-        {
-            get
-            {
-                return new OptimizeClient(getApi());
-            }
-        }
-
-        public StampClient StampClient
-        {
-            get
-            {
-                return new StampClient(getApi());
+                return new ExtractClient(getApi());
             }
         }
 
@@ -73,21 +103,21 @@ namespace Pdf4meClient
             }
         }
 
-        public OcrClient OcrClient
-        {
-            get
-            {
-                return new OcrClient(getApi());
-            }
-        }
-
-        public JobClient JobClient
+        /*public JobClient JobClient
         {
             get
             {
                 return new JobClient(getApi());
             }
-        }
+        }*/
+
+        /*public ManagementClient ManagementClient
+        {
+            get
+            {
+                return new ManagementClient(getApi());
+            }
+        }*/
 
         public MergeClient MergeClient
         {
@@ -97,19 +127,19 @@ namespace Pdf4meClient
             }
         }
 
-        public ConvertClient ConvertClient
+        /*public OcrClient OcrClient
         {
             get
             {
-                return new ConvertClient(getApi());
+                return new OcrClient(getApi());
             }
-        }
+        }*/
 
-        public ExtractClient ExtractClient
+        public OptimizeClient OptimizeClient
         {
             get
             {
-                return new ExtractClient(getApi());
+                return new OptimizeClient(getApi());
             }
         }
 
@@ -129,9 +159,78 @@ namespace Pdf4meClient
             }
         }
 
+        public StampClient StampClient
+        {
+            get
+            {
+                return new StampClient(getApi());
+            }
+        }
+
         public HttpClient getApi()
         {
-            if (_httpClient != null) return _httpClient;
+
+
+            HttpClient client;
+
+            if (!string.IsNullOrEmpty(_clientId) && !string.IsNullOrEmpty(_key))
+            {
+                ClientCredential clientCred = new ClientCredential(_clientId, _key);
+
+                AuthenticationContext authenticationContext = new AuthenticationContext(_authString, false);
+                AuthenticationResult authenticationResult = authenticationContext.AcquireTokenAsync(_clientId, clientCred).ConfigureAwait(false).GetAwaiter().GetResult();
+                var token = authenticationResult.AccessToken;
+                //txtToken.Text = token;
+
+                // Do Stamp
+
+                ServicePointManager.ServerCertificateValidationCallback = delegate { return true; };
+
+                client = new HttpClient();
+                client.Timeout = new TimeSpan(0, 5, 0);
+                client.DefaultRequestHeaders.Add("Authorization", "Bearer " + token);
+                //client.SetBearerToken(token);
+
+                client.DefaultRequestHeaders.Accept.Clear();
+                client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+
+                //client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+
+                Uri apiUri = new Uri(_api);
+                client.BaseAddress = apiUri;
+
+            }
+            else if (!string.IsNullOrEmpty(_basicToken))
+            {
+
+                ServicePointManager.ServerCertificateValidationCallback = delegate { return true; };
+
+                client = new HttpClient();
+                client.Timeout = new TimeSpan(0, 5, 0);
+
+
+                //var byteArray = Encoding.ASCII.GetBytes($"{_clientId}:{_key}");
+                //var byteArray = Encoding.ASCII.GetBytes($"{_basicToken}");
+                //var basicToken = Convert.ToBase64String(byteArray);
+                client.DefaultRequestHeaders.Authorization = new System.Net.Http.Headers.AuthenticationHeaderValue("Basic", _basicToken);
+
+                client.DefaultRequestHeaders.Accept.Clear();
+                client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+
+
+                Uri apiUri = new Uri(_api);
+                client.BaseAddress = apiUri;
+            }
+            else
+                throw new ApplicationException("Missing token for authentication, please give ClientId/Key or BasicToken");
+
+
+
+            return client;
+        }
+
+        public string getApiToken()
+        {
 
             //string tenantName = "devynooxlive.onmicrosoft.com";
             //string authString = "https://login.microsoftonline.com/" + tenantName;
@@ -139,37 +238,23 @@ namespace Pdf4meClient
             // SLApp
             //string clientId = "98a707a7-1860-4bbb-b956-51d95f1f338c";
             //string key = "o6YE76EHPPdnia7h/juHKIdDf7bWYgcu3PbzHuK6qJk=";
-                       
+
 
             //string resource = clientId;
 
 
-            ClientCredential clientCred = new ClientCredential(_authConfig.ClientId, _authConfig.Secret);
+            ClientCredential clientCred = new ClientCredential(_clientId, _key);
 
             string token;
 
             AuthenticationContext authenticationContext = new AuthenticationContext(_authString, false);
-            AuthenticationResult authenticationResult = authenticationContext.AcquireTokenAsync(_authConfig.ClientId, clientCred).ConfigureAwait(false).GetAwaiter().GetResult();
+            AuthenticationResult authenticationResult = authenticationContext.AcquireTokenAsync(_clientId, clientCred).ConfigureAwait(false).GetAwaiter().GetResult();
             token = authenticationResult.AccessToken;
             //txtToken.Text = token;
 
 
-            // Do Stamp
+            return token;
 
-            ServicePointManager.ServerCertificateValidationCallback = delegate { return true; };
-
-            HttpClient client = new HttpClient();
-            client.DefaultRequestHeaders.Add("Authorization","Bearer " + token);
-            //client.SetBearerToken(token);
-
-            Uri apiUri = new Uri(_authConfig.Uri);
-            
-            client.BaseAddress = apiUri;
-
-            client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
-
-
-            return _httpClient = client;
         }
 
 
